1f3884cdea41e33926ef8391a9d1a9b77af38819
Samuel Soares <63384137+SamuCostaSoares@users.noreply.github.com>
2023-04-21 22:01:44 -0300

    commit study the readme file to initiate changes

--- my-portfolio/gatsby-starter-minimal-blog/gatsby-config.ts
+++ my-portfolio/gatsby-starter-minimal-blog/gatsby-config.ts
@@ -8,7 +8,7 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.mjs
-    siteTitle: `Minimal Blog`,
+    siteTitle: `Bem-Vindo`,
     siteTitleAlt: `Minimal Blog - Gatsby Theme`,
     siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
     siteUrl: `https://minimal-blog.lekoarts.de`,
@@ -36,11 +36,11 @@ const config: GatsbyConfig = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `Git Hub`,
+            url: `https://github.com/SamuCostaSoares`,
           },
         ],
       },
